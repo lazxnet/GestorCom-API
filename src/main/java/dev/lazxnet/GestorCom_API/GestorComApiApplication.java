5e1663c700b410bcbdd9528a9562119b4b1b3e3a
Lázaro Campos <lazaromanuel65@gmail.com>
2025-02-07 23:18:38 -0500

    [REFACTOR] preformars

--- src/main/java/dev/lazxnet/GestorCom_API/GestorComApiApplication.java
+++ src/main/java/dev/lazxnet/GestorCom_API/GestorComApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestorComApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GestorComApiApplication.class, args);
 	}
 
